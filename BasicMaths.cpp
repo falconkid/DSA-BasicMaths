496793773235028a2a715506e286039fb7694b28
Abhinav <kidfalcon88@gmail.com>
2026-03-25 23:17:45 -0500

    Removed unnecessary variables for logic, now more clean

--- BasicMaths.cpp
+++ BasicMaths.cpp
@@ -10,12 +10,12 @@ int main() {
     int current = 0;
     int count = 0;
     string reversed = "";
-    bool palindrome = false;
+    
     int copy = n;
     while(copy != 0){
-        int divided = copy/10;
+        
         int lastdigit = copy%10;
-        copy = divided;
+        copy = copy/10;
         current = current +  lastdigit;
         cout << lastdigit ;
         count++;
@@ -24,15 +24,18 @@ int main() {
         
         
     }
-
-    if(reversed == to_string(n)){
-        palindrome = true;
-    }
     cout << endl;
+    if(reversed == to_string(n)){
+        cout << "Palindrome" << endl;
+    }
+    else{
+        cout << "Not Palindrome" << endl;
+    }
+    
     cout << "Sum is: " << current << endl;
     cout << "Count is: " << count << endl;
     cout << "Reversed is: " << reversed << endl;
-    palindrome ? cout << "The number is palindrome" << endl : cout << "Not Palindrome" << endl;
+    
 
     return 0;
 }
