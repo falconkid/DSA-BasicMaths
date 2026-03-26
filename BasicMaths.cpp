36da58982d13ec71f58a35f03a800e37459bf4c5
Abhinav <kidfalcon88@gmail.com>
2026-03-25 23:13:07 -0500

    Added feature to check palindrome or not

--- BasicMaths.cpp
+++ BasicMaths.cpp
@@ -4,24 +4,35 @@ using namespace std;
 
 int main() {
     
-    int n = 1879;
+    int n = 0;
+    cout << "Enter the number: " << endl;
+    cin >> n;
     int current = 0;
     int count = 0;
     string reversed = "";
-    
-    while(n != 0){
-        int divided = n/10;
-        int lastdigit = n%10;
-        n = divided;
+    bool palindrome = false;
+    int copy = n;
+    while(copy != 0){
+        int divided = copy/10;
+        int lastdigit = copy%10;
+        copy = divided;
         current = current +  lastdigit;
-        cout << lastdigit << endl;
+        cout << lastdigit ;
         count++;
         reversed = reversed + to_string(lastdigit);
        
         
+        
     }
-    cout << " Sum is: " << current << endl;
-    cout << " Count is: " << count << endl;
-    cout << " Reversed is: " << reversed << endl;
+
+    if(reversed == to_string(n)){
+        palindrome = true;
+    }
+    cout << endl;
+    cout << "Sum is: " << current << endl;
+    cout << "Count is: " << count << endl;
+    cout << "Reversed is: " << reversed << endl;
+    palindrome ? cout << "The number is palindrome" << endl : cout << "Not Palindrome" << endl;
+
     return 0;
 }
