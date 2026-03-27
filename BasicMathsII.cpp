543a88cc979e22bb86121dcb93b739ade4358096
Abhinav <kidfalcon88@gmail.com>
2026-03-26 22:58:30 -0500

    My initial logic for finding gcd, worked out on first time with some very minor errors

--- BasicMathsII.cpp
+++ BasicMathsII.cpp
@@ -1,22 +1,57 @@
+// #include <iostream>
+// #include<cmath>
+// using namespace std;
+
+// int main() {
+    
+//     int n;
+//     cout << "Enter the number: " << endl;
+//     cin >> n;
+    
+    
+    
+//     for(int i = 1; i <= (n/2); i++) {
+//         if(n%i == 0){
+//             cout << i << endl;
+//         }
+        
+//     }
+    
+
+//     return 0;
+// }
+
+
+
+//GCD of two numbers
+
 #include <iostream>
 #include<cmath>
 using namespace std;
 
+
+int gcd(int a , int b) {
+
+    int highestA = 0;
+    int highestB = 0;
+    for(int i=1; i <= min(a,b); i++){
+        if(a%i == 0 && i > highestA && b%i == 0 && i > highestB ) {
+            highestA = i;
+            highestB = i;
+        }
+    }
+    return highestA , highestB;
+
+}
+
 int main() {
     
-    int n;
-    cout << "Enter the number: " << endl;
-    cin >> n;
-    
-    
-    
-    for(int i = 1; i <= (n/2); i++) {
-        if(n%i == 0){
-            cout << i << endl;
-        }
-        
-    }
-    
+    int a , b;
+    cout << "Enter the first number: " << endl;
+    cin >> a;
+    cout << "Enter the second number: " << endl;
+    cin >> b;
+    cout << "The gcd is: " << gcd(a , b);
 
     return 0;
 }
