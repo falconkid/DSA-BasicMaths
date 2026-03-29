a8ff600144c9a0f35b53efd2e9a8dab0e82c260a
Abhinav <kidfalcon88@gmail.com>
2026-03-29 14:08:45 -0500

    Added some recursive functions for ptinting n numbers and reverse order too

--- Recursion.cpp
+++ Recursion.cpp
@@ -1,27 +1,44 @@
 #include<iostream>
 using namespace std;
 
-int count = 0;
-int f(int n){
 
-    
-    if(count == n){
-        return n;
+void printName(int i , int n){
+
+    if(i > n){
+        return;
     }
-    cout << count;
-    count++;
-    return f(n);
-
-
-    
+    cout << "Name ";
+    return printName(i+1 , n);
 }
     
+void printN(int i  , int n){
+    if(i > n){
+        return;
+    }
+    cout << i ;
+    printN(i+1, n);
+    
+}
 
+void printReverse(int i , int n){
+
+    if(i > n){
+        return;
+    }
+    cout << n;
+    printReverse(i , n-1);
+    
+    
+}
 
 int main(){
-
-    
-    cout << f(4);
+    int n;
+    cout << "Enter n: " << endl;
+    cin >> n;
+    printName(1 , n);
+    printN(1 , n);
+    cout << endl;
+    printReverse(1 , n);
 
     return 0;
     
