2bbee9b5878bb87a65d5cf68c62a3468cb7626a2
Abhinav <kidfalcon88@gmail.com>
2026-03-30 23:51:19 -0500

    Added the bool function to check palindromic string or not palindromic string

--- RecursionII.cpp
+++ RecursionII.cpp
@@ -1,19 +1,66 @@
 #include<iostream>
 using namespace std;
+#include <string>
+#include <cctype>
 
-void sumofN(int arr[] , int n){
-    if( n == 0  ){
-        return;
+// void ReverseofN(int arr[] , int n){
+//     if( n == 0  ){
+//         return;
+//     }
+    
+//     ReverseofN(arr+1 , n-1);
+//     cout << arr[0];
+
+// }
+// int main(){
+//     int n;
+//     int arr[] = {5,1,3,2,3};
+    
+//     ReverseofN(arr , sizeof(arr)/sizeof(arr[0]));
+    
+// }
+
+
+
+
+//----------------Palindrome or not -------------------
+
+
+
+
+
+
+bool isPalindrome(string test1){
+    
+    int start = 0;
+    int end = test1.length()-1;
+    
+    while(start < end){
+        while(start < end && !isalnum(test1[start])){
+            start++;
+        }
+        while(start < end && !isalnum(test1[end])){
+            end--;
+        }
+        while(tolower(test1[start]) != tolower(test1[end])){
+            return false;
+        }
+        start++;
+        end--;
     }
+    return true;
     
-    sumofN(arr+1 , n-1);
-    cout << arr[0];
-
+    
 }
-int main(){
-    int n;
-    int arr[] = {5,1,3,2,3};
+
+
+
+int main() {
+    string test1 = "A man, a plan, a canal: Panama";
+    string test2 = "race a car";
     
-    sumofN(arr , sizeof(arr)/sizeof(arr[0]));
+    cout << (isPalindrome(test1) ? "true" : "false") << endl;  // Expected: true
+    cout << (isPalindrome(test2) ? "true" : "false") << endl;  // Expected: false
     
+    return 0;
 }
